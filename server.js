61afd79978cb2f9639b239ce5910d01e30c5f315
jareer <jareerzeenam@adventus.io>
2021-12-29 16:58:44 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+
+const mongoose = require('mongoose');
+
+async function startServer() {
+  // Initialize app
+  const app = express();
+  const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  await apolloServer.start();
+
+  // /graphql
+  apolloServer.applyMiddleware({ app: app });
+
+  //express middleware
+  app.use((req, res) => {
+    res.send('Hello From Express Apollo Server');
+  });
+
+  // !
+  await mongoose.connect(
+    'mongodb+srv://jareer_28:xijRRSO8b1ptx8Re@cluster0.o5nyg.mongodb.net/data?retryWrites=true&w=majority',
+    {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    }
+  );
+  console.log('Mongoose Connected');
+
+  app.listen(4000, () => console.log('Server Started at 4000'));
+}
+startServer();
